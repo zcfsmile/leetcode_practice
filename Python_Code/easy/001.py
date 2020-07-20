e75164209ebcf4cd2b8e8b70b0d6977ef6fab21c
franklin <985769411@qq.com>
2020-07-20 16:42:35 +0800

    Create 001.py
    001 两数之和 Python3

---
+++ Python_Code/easy/001.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashMap = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in hashMap:
+                return [hashMap[diff], i]
+            hashMap[nums[i]] = i
+        return None
+
+arr = Solution().twoSum([2, 2, 2, 15], 4)
+print(arr)
